5d4db61b0b61cefed4939dd00cc725477cc71985
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-08-21 11:22:33 +0900

    update broker test code base

--- pkg/broker/main_test.go
+++ pkg/broker/main_test.go
@@ -1,25 +1,92 @@
 package broker_test
 
 import (
+	"context"
 	"os"
 	"testing"
+	"time"
 
-	"github.com/joho/godotenv"
+	_ "github.com/Goboolean/shared/internal/util/env"
+	"github.com/Goboolean/shared/pkg/broker"
+	"github.com/Goboolean/shared/pkg/resolver"
 )
+
+
+func SetUp() {
+
+	const (
+		existingTopic = "existing-topic" // this code is assured
+		nonExistentTopic = "non-existent-topic"
+		testTopic = "test-topic"
+		defaultTopic = "default-topic"
+	)
+
+	conf, err := broker.NewConfigurator(&resolver.ConfigMap{
+		"HOST": os.Getenv("KAFKA_HOST"),
+		"PORT": os.Getenv("KAFKA_PORT"),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	defer conf.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	// Verify that "existing-topic" exists, if not, create it
+	exists, err := conf.TopicExists(ctx, existingTopic)
+	if err != nil {
+		panic(err)
+	}
+
+	if !exists {
+		if err := conf.CreateTopic(ctx, existingTopic); err != nil {
+			panic(err)
+		}
+	}
+
+	// Verify that "non-existent-topic" does not exist, if it does, delete it
+	exists, err = conf.TopicExists(ctx, nonExistentTopic)
+	if err != nil {
+		panic(err)
+	}
+
+	if exists {
+		if err := conf.DeleteTopic(ctx, nonExistentTopic); err != nil {
+			panic(err)
+		}
+	}
+
+	// Verify that "test-topic" does not exist, if it does, delete it
+	exists, err = conf.TopicExists(ctx, testTopic)
+	if err != nil {
+		panic(err)
+	}
+
+	if exists {
+		if err := conf.DeleteTopic(ctx, testTopic); err != nil {
+			panic(err)
+		}
+	}
+
+	// Verify that "default-topic" exist, if not, create it
+	exists, err = conf.TopicExists(ctx, defaultTopic)
+	if err != nil {
+		panic(err)
+	}
+
+	if !exists {
+		if err := conf.CreateTopic(ctx, defaultTopic); err != nil {
+			panic(err)
+		}
+	}
+}
 
 
 
 func TestMain(m *testing.M) {
-
-	if err := os.Chdir("../../"); err != nil {
-		panic(err)
-	}
-
-	if err := godotenv.Load(); err != nil {
-		panic(err)
-	}
-
+	SetUp()
 	code := m.Run()
-
 	os.Exit(code)
 }
