21894590ee8f05968013b5e58fc04d0d41b3822c
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-08-22 12:22:33 +0900

    update broker publishser test code

--- pkg/broker/subscriber_test.go
+++ pkg/broker/subscriber_test.go
@@ -8,12 +8,17 @@ import (
 
 	"github.com/Goboolean/shared/pkg/broker"
 	"github.com/Goboolean/shared/pkg/resolver"
-	"google.golang.org/protobuf/proto"
+	"github.com/stretchr/testify/assert"
 )
 
-var (
-	sub *broker.Subscriber
-)
+var sub *broker.Subscriber
+
+type SubscribeListenerImpl struct{}
+var stockChan = make(chan *broker.StockAggregate)
+
+func (i *SubscribeListenerImpl) OnReceiveStockAggs(name string, data *broker.StockAggregate) {
+	stockChan <- data
+}
 
 func SetupSubscriber() {
 	var err error
@@ -21,6 +26,7 @@ func SetupSubscriber() {
 	sub, err = broker.NewSubscriber(&resolver.ConfigMap{
 		"HOST": os.Getenv("KAFKA_HOST"),
 		"PORT": os.Getenv("KAFKA_PORT"),
+		"GROUP": "test",
 	}, context.Background(), &SubscribeListenerImpl{})
 	if err != nil {
 		panic(err)
@@ -31,45 +37,33 @@ func TeardownSubscriber() {
 	sub.Close()
 }
 
+
+
 func Test_Subscriber(t *testing.T) {
 
 	SetupSubscriber()
+	defer TeardownSubscriber()
 
-	ctx, cancelFunc := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancelFunc()
+	t.Run("Ping", func(t *testing.T) {
+		ctx, cancelFunc := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancelFunc()
 
-	if err := sub.Ping(ctx); err != nil {
-		t.Errorf("Ping() failed: %v", err)
-	}
-
-	TeardownSubscriber()
+		err := sub.Ping(ctx)
+		assert.NoError(t, err)
+	})
 }
 
-type SubscribeListenerImpl struct{}
 
-var stockChan = make(chan *broker.StockAggregate)
-
-func (i *SubscribeListenerImpl) OnReceiveStockAggs(name string, data *broker.StockAggregate) {
-	stockChan <- data
-}
 
 func Test_Subscribe(t *testing.T) {
 
 	var topic = "test-topic"
+	
 	SetupSubscriber()
 	SetupPublisher()
-	SetupConfigurator()
+	defer TeardownSubscriber()
+	defer TeardownPublisher()
 
-	ctx, cancelFunc := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancelFunc()
-
-	exists, err := conf.TopicExists(ctx, topic)
-	if err != nil {
-		t.Errorf("failed to check topic exists: %v", err)
-	}
-	if !exists {
-		t.Errorf("topic does not exist")
-	}
 
 	type args struct {
 		topic string
@@ -82,7 +76,7 @@ func Test_Subscribe(t *testing.T) {
 		want *broker.StockAggregate
 	}{
 		{
-			name: "send mock data",
+			name: "",
 			args: args{
 				topic: topic,
 				data: &broker.StockAggregate{
@@ -97,36 +91,26 @@ func Test_Subscribe(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+			defer cancel()
 
-			if err := sub.Subscribe(topic); err != nil {
-				t.Errorf("Subscribe() = %v", err)
-			}
+			err := sub.Subscribe(topic)
+			assert.NoError(t, err)
 
-			if err := pub.SendData(tt.args.topic, tt.args.data); err != nil {
-				t.Errorf("SendData() = %v", err)
-			}
+			err = pub.SendData(tt.args.topic, tt.args.data)
+			assert.NoError(t, err)
 
 			select {
 			case <-ctx.Done():
 				t.Errorf("timeout: failed to receive data")
 			case got := <-stockChan:
-				if !proto.Equal(got, tt.want) {
-					t.Errorf("OnReceiveStockAggs() = %v, want %v", got, tt.want)
-				}
+				assert.Equal(t, tt.want, got)
 			}
-
-			cancel()
 		})
 	}
 
-	if err := pub.SendData(topic, data); err != nil {
-		t.Errorf("NewPublisher() failed: %v", err)
-	}
-
-	close(stockChan)
-
-	TeardownSubscriber()
-	TeardownPublisher()
-	TeardownConfigurator()
+	t.Run("SubscribeNonExistantTopic", func(t *testing.T) {
+		err := sub.Subscribe("non-existent-topic")
+		assert.Error(t, err)
+	})
 }
