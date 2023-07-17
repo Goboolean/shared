72b055228f03c7b9d8af223913d758899610013a
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-07-17 14:58:05 +0900

    fix schema and queries

--- api/sql/queries.sql
+++ api/sql/queries.sql
@@ -1,28 +1,38 @@
 -- name: CreateAccessInfo :exec
 INSERT INTO store_log (product_id, "status") VALUES ($1, $2);
 
+
 -- name: InsertNewStockMeta :exec
-INSERT INTO product_meta (product_id, "name", symbol, "description", "type", exchange, "location") 
+INSERT INTO product_meta (id, "name", symbol, "description", "type", exchange, "location") 
 VALUES ($1, $2, $3, $4, $5, $6, $7);
 
 -- name: InsertNewStockPlatformMeta :exec
-INSERT INTO stock_platform (product_id, platform, identifier) VALUES ($1, $2, $3);
+INSERT INTO product_platform (product_id, platform_name, identifier)
+VALUES ($1, $2, $3);
+
 
 -- name: CheckStockExist :one
-SELECT EXISTS(SELECT 1 FROM product_meta WHERE product_id = ($1));
+SELECT EXISTS(SELECT 1 FROM product_meta WHERE id = ($1));
 
 -- name: GetStockMeta :one
-SELECT product_id, "name", symbol, "description", "type", exchange,  "location"  FROM product_meta WHERE product_id = ($1);
+SELECT id, "name", symbol, "description", "type", exchange,  "location"  FROM product_meta WHERE id = ($1);
 
 -- name: GetAllStockMetaList :many
-SELECT product_id, "name", symbol, "description", "type", exchange,  "location"  FROM product_meta;
+SELECT id, "name", symbol, "description", "type", exchange,  "location"  FROM product_meta;
 
 -- name: GetStockMetaWithPlatform :one
-SELECT product_meta.product_id, "name", symbol, "description", "type", exchange,  "location" , platform, identifier 
+SELECT product_meta.id, "name", symbol, "description", "type", exchange,  "location" , platform_name, identifier 
 FROM product_meta 
-JOIN stock_platform 
-ON product_meta.product_id = stock_platform.product_id 
-WHERE product_meta.product_id = ($1);
+JOIN product_platform 
+ON product_meta.id = product_platform.product_id 
+WHERE product_meta.id = ($1);
 
--- name: UpdatePlatform :exec
-UPDATE stock_platform SET platform = ($1), identifier = ($2) WHERE product_id = ($3);
+-- name: UpdatePlatformIdentifier :exec
+UPDATE product_platform SET identifier = ($1) WHERE product_id = ($2) AND platform_name = ($3);
+
+-- name: DeletePlatformInfo :exec
+DELETE FROM product_platform WHERE product_id = ($1) AND platform_name = ($2);
+
+-- name: InsertPlatformInfo :exec
+INSERT INTO product_platform (product_id, platform_name, identifier) VALUES ($1, $2, $3);
+
--- api/sql/schema.sql
+++ api/sql/schema.sql
@@ -1,26 +1,33 @@
--- metadata table for product data
+/* metadata table for product data */
 CREATE TABLE product_meta (
-  product_id    varchar(32) NOT NULL, /* an identifier form:{type}.{name}.{location}*/
-  "name"        varchar(32) NOT NULL, /* human readable stock name */
-  symbol        varchar(8)  NOT NULL, /* stock symbol */
-  "description" text,                 /* product description */
-  "type"        varchar(8)  NOT NULL, /* examples are stock, encrypt */
-  exchange      varchar(32) NOT NULL, /* examples are kospi, nasdaq. */
-  "location"    varchar(32),          /* examples are kor, usa. when product type is coin location is null*/
-  PRIMARY KEY (product_id)
+  id          varchar(32) NOT NULL, /* an identifier form : {type}.{name}.{location} */
+  name        varchar(32) NOT NULL, /* human readable stock name */
+  symbol      varchar(8)  NOT NULL, /* stock symbol */
+  description text,                 /* product description : can be gain by external apis, or written manually by admin */
+  type        varchar(8)  NOT NULL, /* examples are stock, encrypt */
+  exchange    varchar(32) NOT NULL, /* examples are kospi, nasdaq. */
+  location    varchar(32),          /* examples are kor, usa. when product type is coin location is null*/
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE stock_platform (
-  product_id varchar(32) NOT NULL, /* an product identifier form:{type}.{name}.{location}*/
-  platform   varchar(16) NOT NULL, /* available platform is buycycle, polygon, kis */
-  identifier varchar(32) NOT NULL, /* a string that is used to specific stock on such platform query */
+CREATE TABLE platform (
+  name        varchar(32) NOT NULL, /* available platform is buycycle, polygon, kis */
+  description text,
+  PRIMARY KEY (name)
+);
 
-  PRIMARY KEY (product_id),
-  FOREIGN KEY (product_id) REFERENCES product_meta (product_id)
+CREATE TABLE product_platform (
+  product_id    varchar(32) NOT NULL,
+  platform_name SERIAL      NOT NULL, 
+  identifier    varchar(32) NOT NULL, /* a string that is used to specific stock on such platform query */
+
+  PRIMARY KEY (product_id, platform),
+  FOREIGN KEY (product_id) REFERENCES product_meta (id),
+  FOREIGN KEY (platform_name) REFERENCES platform (name)
 );
 
 CREATE TABLE store_log (
-  product_id  varchar(32) NOT NULL, /* an product identifier form:{type}.{name}.{location}*/
+  product_id  varchar(32) NOT NULL,
   stored_at   timestamp   NOT NULL DEFAULT CURRENT_TIMESTAMP,
   "status"    varchar(10) NOT NULL,
 
